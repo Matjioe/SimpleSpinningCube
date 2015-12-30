f786a77a56c71bc84ada5518b3525f3b2ffbe3e1
Mathieu Muller <mathieu@unity3d.com>
2015-12-30 19:41:48 +0100

    Increase memory + Material switch

---
+++ Assets/SwitchTexture.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchTexture : MonoBehaviour {
+
+    public Material[] materials;
+    private int currentMaterialIdx = 0;
+
+    public void SwitchMaterial()
+    {
+        currentMaterialIdx++;
+        if (currentMaterialIdx == materials.Length)
+            currentMaterialIdx = 0;
+        GetComponent<MeshRenderer>().sharedMaterial = materials[currentMaterialIdx];
+    }
+	
+}
